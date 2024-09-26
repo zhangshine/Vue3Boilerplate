dc61edcc6b7f454de3908280ec627c1c07d332f3
zhangshine <zhangshine0125@gmail.com>
2024-09-26 21:37:20 +0800

    enable lint when building target

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false,
+  // if set to false it will not run eslint when building to target
+  lintOnSave: 'default',
   productionSourceMap: false,
 })
